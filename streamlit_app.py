f9d18d11ce7b10d3ff9ad207f0bfec4288f4c786
Nathan Hawes <nathanhawes@users.noreply.github.com>
2025-01-05 08:51:37 +0000

    Update streamlit_app.py
    removed cr

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '  
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
