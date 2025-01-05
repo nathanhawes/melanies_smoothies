ba23136b9b6e95bfc03d0d37d57e6c0194bbf160
Nathan Hawes <nathanhawes@users.noreply.github.com>
2025-01-05 08:54:56 +0000

    Update streamlit_app.py
    Nutrition information per fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '  
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        ingredients_string += fruit_chosen +' '
+        st.subheader(fruit_chosen +'Nutrition information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(my_insert_stmt)
