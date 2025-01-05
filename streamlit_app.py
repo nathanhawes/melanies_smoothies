56635167b1d7b9d21c339233fc8672b7ca6cf180
Nathan Hawes <nathanhawes@users.noreply.github.com>
2025-01-05 09:32:16 +0000

    Update streamlit_app.py
    space in header

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(fruit_chosen +'Nutrition information')
+        st.subheader(fruit_chosen +' Nutrition information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +search_on)
         sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
