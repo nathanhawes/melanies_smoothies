5e984783cd2bb02bbc92f941c586e8884ba0536c
Nathan Hawes <nathanhawes@users.noreply.github.com>
2025-01-05 08:30:13 +0000

    Update streamlit_app.py
    New section added to display nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success("You smoothie is ordered, "+name_on_order, icon="✅")
+
+# New section to display nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
